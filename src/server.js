0c48e7dcc2098e6c55c738d5b1125b34176722e6
Rosiele Serpa <rosieleserpa@hotmail.com>
2020-08-14 12:07:28 -0300

    refactor: data moved to pages

--- src/server.js
+++ src/server.js
@@ -1,31 +1,3 @@
-const { subjects, weekdays, getSubject } = require('./utils/format')
-
-function pageLanding(req,res) {
-  return res.render("index.html")
-}
-
-function pageStudy(req,res) {
-  const filters = req.query
-  return res.render("study.html", { proffys, filters, subjects, weekdays })
-}
-
-function pageGiveClasses (req,res) {
-  const data = req.query
-
-  // Verifica se os dados estão vazios
-  const isNotEmpty = Object.keys(data).length > 0
-
-  if (isNotEmpty) {
-    data.subject = getSubject(data.subject)
-
-    // Adcionar dados (data) a lista de proffys
-    proffys.push(data)
-
-    return res.redirect("/study")
-  }
-
-  return res.render("give-classes.html", { subjects, weekdays })
-}
 // Servidor
 const express = require('express')
 const server = express()
