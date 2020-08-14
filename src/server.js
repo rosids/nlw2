2551dd7b706c09307e931176cebae8fea9556556
Rosiele Serpa <rosieleserpa@hotmail.com>
2020-08-14 12:02:45 -0300

    refactor: data moved to utils/format

--- src/server.js
+++ src/server.js
@@ -1,33 +1,3 @@
-const subjects = [
-  "Artes",
-  "Biologia",
-  "Ciências",
-  "Educação Física",
-  "Física",
-  "Geografia",
-  "História",
-  "Matemática",
-  "Português",
-  "Química",
-]
-
-const weekdays = [
-  "Domingo",
-  "Segunda-feira",
-  "Terça-feira",
-  "Quarta-feira",
-  "Quinta-feira",
-  "Sexta-feira",
-  "Sábado",
-]
-
-// Funcionalidades
-
-function getSubject(subjectNumber) {
-  const position = +subjectNumber - 1
-  return subjects[position]
-}
-
 function pageLanding(req,res) {
   return res.render("index.html")
 }
