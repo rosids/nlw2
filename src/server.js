28cb0f8d0217ebeda8e71a1fac2674d38f77d919
Rosiele Serpa <rosieleserpa@hotmail.com>
2020-08-14 14:55:57 -0300

    feat: add post

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const server = express()
 
-const  { pageLanding, pageStudy, pageGiveClasses } = require('./pages')
+const  { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require('./pages')
 
 // Configurar nunjucks (template engine)
 const nunjucks = require("nunjucks")
@@ -13,12 +13,15 @@ nunjucks.configure("src/views", {
 
 // Início e configuração do servidor
 server
+// Receber os dados do req.body
+.use(express.urlencoded({ extended:true }))
 // Configurando arquivos estáticos: css, scripts, imagens
 .use(express.static("public"))
 // Rotas da aplicação
 .get("/", pageLanding)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
 
 // Start do servidor
 .listen(5500)
